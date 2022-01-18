2f7054b081b832665a67e727ba917c5f71096a72
leilla-pecqueux <leilla.pecqueux@gmail.com>
2022-01-18 18:52:19 +0100

    maj

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var toggle = document.querySelector('#toggle')
+var container = document.querySelector('#container')
+
+toggle.addEventListener('click', function(){
+container.classList.toggle('block');
+}
+)
+
